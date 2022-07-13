aa5f3bf4cd99eaa791bd4393014d00ec207dc283
Carbonateds <90555321+Carbonateds@users.noreply.github.com>
2022-07-14 00:05:55 +0800

    Add the Main Class of the Mod.

---
+++ src/main/java/com/carbonateds/tutorial/Tutorial.java
@@ -0,0 +1,15 @@
+package com.carbonateds.tutorial;
+
+import net.minecraftforge.fml.common.Mod;
+
+/**
+ * @author Carbonate
+ * The Main Class of the Example Mod of the Forge Mod Development Tutorial.
+ */
+
+@Mod(Tutorial.MODID)
+public class Tutorial
+{
+    //Set a Constant to store the Modid of the Mod.
+    public static final String MODID = "tutorial";
+}
